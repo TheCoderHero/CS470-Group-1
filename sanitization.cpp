6e24c5a9d31090d4416d97899c644ae1f9e3f32b
Jordon Thompson <tho17088@byui.edu>
2020-02-10 21:31:07 -0700

    stub out functions

--- sanitization.cpp
+++ sanitization.cpp
@@ -18,3 +18,62 @@
  *    3. What assumptions about input (file or user) is made
  *    4. Citations if the algorithm was borrowed or inspired from any source
  ************************************************************************/
+#include <iostream>
+#include <string>
+using namespace std;
+
+void promptUser(string *username, string *password);
+string queryGeneration(string *username, string *password);
+void testVulenrabilities(string results);
+void weakMitigation(string tuatology);
+void strongMitigation(string cmdInjection);
+
+/*****************************************************
+ * 
+ * 
+ * **************************************************/
+int main() {
+
+
+   return 0;
+}
+
+/*****************************************************
+ * 
+ * 
+ * **************************************************/
+void promptUser(string *username, string *password){
+
+}
+
+/*****************************************************
+ * 
+ * 
+ * **************************************************/
+string queryGeneration(string *username, string *password){
+
+}
+
+/*****************************************************
+ * 
+ * 
+ * **************************************************/
+void testVulenrabilities(string results){
+
+}
+
+/*****************************************************
+ * 
+ * 
+ * **************************************************/
+void weakMitigation(string tuatology){
+
+}
+
+/*****************************************************
+ * 
+ * 
+ * **************************************************/
+void strongMitigation(string cmdInjection){
+
+}
