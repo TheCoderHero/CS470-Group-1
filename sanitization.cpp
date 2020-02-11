9bbc69cf14ad477b853816e3ea0b010523d6f249
TheCoderHero <thecoderhero@gmail.com>
2020-02-11 10:54:40 -0700

    Minor edit

--- sanitization.cpp
+++ sanitization.cpp
@@ -79,7 +79,7 @@ string queryGeneration(string username, string password){
  * 
  * **************************************************/
 void testVulnerabilities(string sqlString){
-   // I don't think we need this function - John Tanner
+s
 }
 
 /*****************************************************
