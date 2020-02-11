1117495251e4214d686a80e444386f62eded879e
Osvaldo <osvaldofabricio11@gmail.com>
2020-02-10 19:17:18 -0500

    template

--- sanitizationLab06/main.cpp
+++ sanitizationLab06/main.cpp
@@ -13,3 +13,71 @@
 *
 * Summary:
  ************************************************************************/
+
+#include <iostream>
+
+void queryGeneration(string username, string password);
+void feedQueryGeneration();
+void  feedVulnerabilitiesTautology();
+void  feedVulnerabilitiesUnion();
+void  feedVulnerabilitiesStatement();
+void  feedVulnerabilitiesComment();
+
+int main()
+{
+
+}
+
+/**************************************************
+ * QUERY GENERATION
+ * This function prompts for the username and password
+ * and return a string based on the authentication result.
+ * ************************************************/
+void queryGeneration(string username, string password) {
+
+}
+
+/**************************************************
+ * QUERY GENERATION TEST CASES
+ * This function feeds the test cases of the
+ * query generation function
+ * ************************************************/
+void feedQueryGeneration() {
+
+}
+
+/**************************************************
+ * FEED VULNERABILITIES
+ * This function feeds the test cases that perform the
+ * following attack: tautology.
+ * ************************************************/
+void  feedVulnerabilitiesTautology() {
+
+}
+
+/**************************************************
+ * FEED VULNERABILITIES
+ * This function feeds the test cases that perform the
+ * following attack: union query attack.
+ * ************************************************/
+void  feedVulnerabilitiesUnion() {
+
+}
+
+/**************************************************
+ * FEED VULNERABILITIES
+ * This function feeds the test cases that perform the
+ * following attack:additional statement attack
+ * ************************************************/
+void  feedVulnerabilitiesStatement() {
+
+}
+
+/**************************************************
+ * FEED VULNERABILITIES
+ * This function feeds the test cases that perform the
+ * following attacks: comment attack
+ * ************************************************/
+void  feedVulnerabilitiesComment() {
+
+}
