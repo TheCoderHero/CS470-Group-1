6fb546e4c76027142ce8bd80c9fe8266d68af605
Osvaldo <osvaldofabricio11@gmail.com>
2020-04-04 03:41:26 -0500

    Cipher #5 encrypt and decrypt

--- week-13/Original/cipher05.h
+++ week-13/Original/cipher05.h
@@ -7,67 +7,121 @@
 #define CIPHER05_H
 #include <string>
 #include "cipher.h"
+string generateKey(string str, string key);
+
 /********************************************************************
  * CLASS
  *******************************************************************/
 class Cipher05 : public Cipher
 {
 public:
-   virtual std::string getPseudoAuth()  { return "pseudocode author"; }
-   virtual std::string getCipherName()  { return "cipher name"; }
-   virtual std::string getEncryptAuth() { return "encrypt author"; }
-   virtual std::string getDecryptAuth() { return "decrypt author"; }
+    virtual std::string getPseudoAuth() { return "pseudocode author"; }
+    virtual std::string getCipherName() { return "Vigenère Cipher"; }
+    virtual std::string getEncryptAuth() { return "Osvaldo Carrillo"; }
+    virtual std::string getDecryptAuth() { return "decrypt author"; }
 
-   /***********************************************************
-    * GET CIPHER CITATION
-    * Returns the citation from which we learned about the cipher
-    ***********************************************************/
-   virtual std::string getCipherCitation()
-   {
-      return std::string("citation");
-   }
-   
-   /**********************************************************
-    * GET PSEUDOCODE
-    * Returns the pseudocode as a string to be used by the caller.
+    /***********************************************************
+     * GET CIPHER CITATION
+     * Returns the citation from which we learned about the cipher
+     ***********************************************************/
+    virtual std::string getCipherCitation()
+    {
+        return std::string("citation");
+    }
+
+    /**********************************************************
+     * GET PSEUDOCODE
+     * Returns the pseudocode as a string to be used by the caller.
+     **********************************************************/
+    virtual std::string getPseudocode()
+    {
+        std::string str;
+
+        // TODO: please format your pseudocode
+        // The encrypt pseudocode
+        str = "insert the encryption pseudocode\n";
+
+        // The decrypt pseudocode
+        str += "insert the decryption pseudocode\n";
+
+        return str;
+    }
+
+    /**********************************************************
+    * SAVED KEY
+    * This cipher return a key which the user has to use as the
+    * password to decode the message. In this scenario, this
+    * variable is store globaly to recreate the desire effect.
     **********************************************************/
-   virtual std::string getPseudocode()
-   {
-      std::string str;
+    string savedKey;
+    /**********************************************************
+     * ENCRYPT
+     * TODO: ADD description
+     **********************************************************/
+    virtual std::string encrypt(const std::string& plainText,
+        const std::string& password)
+    {
+        std::string cipherText;
+        // TODO - Add your code here
 
-      // TODO: please format your pseudocode
-      // The encrypt pseudocode
-      str =  "insert the encryption pseudocode\n";
+        //string cipher_text;
+        string keyPassword = generateKey(plainText, password);
 
-      // The decrypt pseudocode
-      str += "insert the decryption pseudocode\n";
+        for (int i = 0; i < plainText.size(); i++)
+        {
+            // converting in range 0-25 
+            int x = (plainText[i] + keyPassword[i]) % 26;
 
-      return str;
-   }
+            // convert into alphabets(ASCII) 
+            x += 'A';
 
-   /**********************************************************
-    * ENCRYPT
-    * TODO: ADD description
-    **********************************************************/
-   virtual std::string encrypt(const std::string & plainText,
-                               const std::string & password)
-   {
-      std::string cipherText = plainText;
-      // TODO - Add your code here
-      return cipherText;
-   }
+            cipherText.push_back(x);
+        }
 
-   /**********************************************************
-    * DECRYPT
-    * TODO: ADD description
-    **********************************************************/
-   virtual std::string decrypt(const std::string & cipherText,
-                               const std::string & password)
-   {
-      std::string plainText = cipherText;
-      // TODO - Add your code here
-      return plainText;
-   }
+        savedKey = keyPassword;
+        return cipherText;
+    }
+
+    /**********************************************************
+     * DECRYPT
+     * TODO: ADD description
+     **********************************************************/
+    virtual std::string decrypt(const std::string& cipherText,
+        const std::string& password)
+    {
+        std::string plainText;
+
+        for (int i = 0; i < cipherText.size(); i++)
+        {
+            // converting in range 0-25 
+            int x = (cipherText[i] - savedKey[i] + 26) % 26;
+
+            // convert into alphabets(ASCII) 
+            x += 'A';
+            plainText.push_back(x);
+        }
+
+        return plainText;
+    }
+
+
+    // This function generates the key in 
+ // a cyclic manner until it's length isi'nt 
+ // equal to the length of original text 
+    string generateKey(string str, string key)
+    {
+        int x = str.size();
+
+        for (int i = 0; ; i++)
+        {
+            if (x == i)
+                i = 0;
+            if (key.size() == str.size())
+                break;
+            key.push_back(key[i]);
+        }
+        return key;
+    }
 };
 
 #endif // CIPHER05_H
